20e913e7bfec449e5435baffe6d4d1e745c40dda
Vivian Wan <vivianpswan@gmail.com>
2020-04-27 16:03:21 +1000

    rendered inputfields

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,31 +8,43 @@ export default class MadlibzForm extends Component {
       this.state = {
         templates: [],
         title: '',
-        inputFields: [],
+        blanks: [],
+        inputFields: [], // do i need this?
       };
     }
 
     componentDidMount() {
       // console.log ('mounted');
       Madlibz.getRandomMadlibTemplate().then(result => {
-        const madlibzTemplate = result.data
-        this.setState({ title: madlibzTemplate.title })
-        this.setState({ inputFields: madlibzTemplate.blanks })
-        console.log ( result.data.blanks.length )
-        // console.log ( result.data.value )
-        // this.setState({ story: madlibzTemplate.value }) // for the story
+        const templates = result.data
+        this.setState({ title: templates.title })
+        this.setState({ blanks: templates.blanks })
+        console.log ( this.state.title )
+    
+        console.log ( templates )
+        console.log ( templates.blanks )
+        console.log ( this.state.blanks )
+        console.log ( templates.blanks.length )
       });
     }
   
     render() {
+
       return (
         <container>
           <div>
             <h5> {this.state.title}</h5>
-            <h6> {this.state.inputFields[0]} </h6>
-            <h6> {this.state.inputFields[1]} </h6>
-            <h6> {this.state.inputFields[2]} </h6>
-            <h6> {this.state.inputFields[3]} </h6>
+            <form>
+              <div>
+                  <ul>
+                    { this.state.blanks.map(((blank, index) => 
+                      <li key={index}>{blank}
+                        <input type="name" />
+                      </li>
+                    ))}
+                  </ul>
+              </div> 
+            </form>
           </div>
         </container>
       )
